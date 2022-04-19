44e3a079df3f07e442aa76d099fe36b64444924a
Alexander Zheng <alexz957@live.unc.edu>
2022-04-19 11:02:42 -0400

    no big changes

--- src/event.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const navigation = document.querySelector('.navigation');
-menuToggle.onclick = () => {
-  navigation.classList.toggle('open');
-}
-
